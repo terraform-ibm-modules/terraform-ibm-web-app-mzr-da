9f2bdba96d0e6046033f2a44ff887b44120178c8
Adam Geiger <ageiger@us.ibm.com>
2024-05-29 13:03:14 -0400

    add resource tags variable

--- solutions/e2e/main.tf
+++ solutions/e2e/main.tf
@@ -3,6 +3,7 @@ module "vpc_vsi_multizone" {
   ibmcloud_api_key                       = var.ibmcloud_api_key
   region                                 = var.region
   prefix                                 = var.prefix
+  resource_tags                          = var.resource_tags
   ssh_key                                = var.ssh_key
   ssh_private_key                        = var.ssh_private_key
   sample_application                     = var.sample_application
