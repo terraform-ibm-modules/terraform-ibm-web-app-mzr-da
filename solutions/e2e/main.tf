4e1650ac8cafd8adcc47355b3ce3d2b7abd96bfe
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-01-31 12:20:35 +0000

    fix(deps): update dependencies (#105)

--- solutions/e2e/main.tf
+++ solutions/e2e/main.tf
@@ -68,7 +68,7 @@ module "private_secret_engine" {
   depends_on                = [ibm_resource_instance.secrets_manager]
   count                     = (var.use_sm && var.existing_sm_instance_guid == null) ? 1 : 0
   source                    = "terraform-ibm-modules/secrets-manager-private-cert-engine/ibm"
-  version                   = "1.12.12"
+  version                   = "1.12.13"
   secrets_manager_guid      = local.sm_guid
   region                    = local.sm_region
   root_ca_name              = var.root_ca_name
